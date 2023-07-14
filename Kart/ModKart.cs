c5eff30a7e9c144772b106a6c16bfcd4c8ee6acf
gamegam(Kor) <48847058+gamegam@users.noreply.github.com>
2023-07-15 03:25:58 +0900

    Add files via upload

---
+++ Kart/ModKart.cs
@@ -0,0 +1,20 @@
+using Android.System;
+using UnityEngine;
+
+namespace Kart;
+
+public class ModKart
+{
+    public  static UnityEngine $untity;
+
+
+        public override getName(string $name)
+    {
+        return untity::getName($name);
+    }
+
+   public override string getDisplayName(string name)
+    {
+    
+    }
+}
